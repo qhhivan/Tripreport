5b5afd3802e26e6c7bd5a64f3dd8a4bd667a32c0
qhhivan <orsolic.i02@htlwienwest.at>
2022-02-18 12:43:13 +0100

    routes
    fertig

--- server/routes/tripreport.js
+++ server/routes/tripreport.js
@@ -1,24 +1,33 @@
 const express = require('express');
 const {
-  getTrainings,
-  getTeilnehmerWithTraining,
-  deleteTeilnehmer,
-  postTeilnehmer,
-  getTeilnehmers,
-  postTraining,
-  getTeilnehmerAnzahlUndStatus,
-  deleteTraining,
+  getCitys,
+  getAttractions,
+  getTrips,
+  getPictures,
+  getPicture,
+  getTrip,
+  deleteTrip,
+  deletePicture,
+  postTrip,
+  postPicture,
+  patchTrip,
 } = require('../controllers/tripreport');
 
 const router = express.Router();
 
-router.get('/teilnehmer', getTeilnehmers);
-router.get('/trainings', getTrainings);
-router.get('/training/:trainingsid', getTeilnehmerWithTraining);
-router.delete('/teilnehmer/:id', deleteTeilnehmer);
-router.delete('/training/:id', deleteTraining);
-router.post('/teilnehmer', postTeilnehmer);
-router.post('/training', postTraining);
-router.get('/trainingteilnehmer/:id', getTeilnehmerAnzahlUndStatus);
+router.get('/citys', getCitys);
+router.get('/attractions', getAttractions);
+router.get('/trips', getTrips);
+router.get('/pictures', getPictures);
+router.get('/pictures/:id', getPicture);
+router.get('/trips/:id', getTrip);
+
+router.delete('/trips/:id', deleteTrip);
+router.delete('/pictures/:id', deletePicture);
+
+router.post('/trips', postTrip);
+router.post('/pictures', postPicture);
+
+router.patch('/trips/:id', patchTrip);
 
 module.exports = router;
